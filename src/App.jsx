ae39475c863efdc501f4569dca3d6822f5c149bc
kibuikaCodes <kibuikasteve@gmail.com>
2023-03-12 16:40:49 +0300

    minor styling configurations

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-import Tesseract from "tesseract.js";
 import axios from "axios";
+import { useState } from "react";
+import Tesseract from "tesseract.js";
+import "./App.css";
 import CloudinaryUploadWidget from "./CloudinaryUpload";
 
 function App() {
@@ -12,32 +11,19 @@ function App() {
   const [queryText, setQueryText] = useState("");
   const [fetching, setFetching] = useState(false);
 
-  // const uploadImage = (e) => {};
-
-  // const handleAdd = (event) => {
-  //   const img = event.target.files?.[0];
-  //   const imageUrl = URL.createObjectURL(img);
-  //   console.log(imageUrl);
-  //   setImage(imageUrl);
-  // };
-
   const recognize = () => {
-    Tesseract.recognize(
-      `${image}`,
-      "eng",
-      {
-        logger: (m) => {
-          // console.log(m);
-          setStatus(m);
-        },
-      }
-    ).then(({ data: { text } }) => {
+    Tesseract.recognize(`${image}`, "eng", {
+      logger: (m) => {
+        // console.log(m);
+        setStatus(m);
+      },
+    }).then(({ data: { text } }) => {
       const cleanText = text.replace(/[^a-zA-Z0-9 ]/g, "");
-      setQueryText(cleanText);      
+      setQueryText(cleanText);
     });
   };
 
-  const getAnswers = () => {
+  const getAnswers = async () => {
     setFetching(true);
     let data = JSON.stringify({
       question: queryText,
@@ -55,70 +41,99 @@ function App() {
       mode: "no-cors",
     };
 
-    axios(config)
-      .then(function (response) {
-        const res = response.data;
-        setAnswer(res?.message?.content);
-        setFetching(false);
-      })
-      .catch(function (error) {
-        console.log(error);
-        setFetching(false);
-      });
+    try {
+      const res = await axios(config);
+      setAnswer(res?.data?.message?.content);
       setFetching(false);
-  }
-
-  
+    } catch (err) {
+      console.log(err);
+      setFetching(false);
+    } finally {
+      setFetching(false);
+    }
+    setFetching(false);
+  };
 
   return (
     <div className="App">
       <h1>Tusomeni: Image to Answers</h1>
       <div className="card">
         <div className="button-wrap btn-logo-box">
-          {/* <input
-            name="paperImage"
-            id="upload"
-            type="file"
-            multiple={false}
-            accept="image/png, image/jpeg"
-            onChange={handleAdd}
-            class="custom-file-input"
-          /> */}
-          {answer ? <button onClick={() => window.location.reload()}>reset</button>: <CloudinaryUploadWidget setImage={setImage}/>}
+          {answer ? (
+            <button onClick={() => window.location.reload()}>reset</button>
+          ) : (
+            <CloudinaryUploadWidget setImage={setImage} />
+          )}
         </div>
-        
-        {(image && !answer) && (
+
+        {image && !answer && (
           <>
             <div>
-              <img
-                src={image}
-                alt="paper image"
-                style={{ marginTop: "3em" }}
-              />
-              <p>*Tip - make sure the image is upright and of good quality and that text can be well read</p>
+              <img src={image} alt="paper image" style={{ marginTop: "3em" }} />
+              <p>
+                *Tip - make sure the image is upright and of good quality and
+                that text can be well read
+              </p>
             </div>
             <button onClick={recognize} style={{ marginTop: "1em" }}>
-              {"Extract Questions"} {status?.progress ? Math.round(status?.progress*100)+"%" : ""}
+              {"Extract Questions"}{" "}
+              {status?.progress ? Math.round(status?.progress * 100) + "%" : ""}
             </button>
           </>
         )}
-        <div style={{marginTop: "1em"}}>
-        {queryText && (
-          <>
-          <textarea cols="40" rows="15" value={queryText} onChange={(e) => setQueryText(e.target.value)} style={{border: "1px solid grey", borderRadius: "5px", padding: ".5em"}}></textarea>
-          <p>* make sure the text is sensible. If not, edit it to what it is supposed to be</p>
-          <button onClick={getAnswers} style={{ marginTop: "1em" }}>
-              {fetching ? "Fetching Answers..." : "Get Answers"}
-            </button>
-          </>
-        )}
-        <div style={{marginTop: "1em"}}>
-        {answer && (
-              <textarea cols="40" rows="15" value={answer} style={{border: "1px solid grey", borderRadius: "5px", padding: ".5em"}}></textarea>
+        <div style={{ marginTop: "1em" }}>
+          {queryText && (
+            <>
+              <textarea
+                cols="40"
+                rows="15"
+                value={queryText}
+                onChange={(e) => setQueryText(e.target.value)}
+                style={{
+                  border: "1px solid grey",
+                  borderRadius: "5px",
+                  padding: ".5em",
+                }}
+              ></textarea>
+              <p>
+                * make sure the text is sensible. If not, edit it to what it is
+                supposed to be
+              </p>
+              {answer && (
+                <p>* you can edit the text to get different answers</p>
+              )}
+
+              <button onClick={getAnswers} style={{ marginTop: "1em" }}>
+                {fetching ? "Fetching Answers..." : "Get Answers"}
+              </button>
+            </>
+          )}
+          <div style={{ marginTop: "1em" }}>
+            {answer && (
+              <textarea
+                cols="40"
+                rows="15"
+                value={answer}
+                style={{
+                  border: "1px solid grey",
+                  borderRadius: "5px",
+                  padding: ".5em",
+                }}
+              ></textarea>
             )}
+          </div>
         </div>
-        </div>
-        
+        <footer
+          style={{
+            position: "relative",
+            bottom: "0",
+            padding: ".5em",
+            marginTop: "3em",
+          }}
+        >
+          <hr />
+          Built by <a href="https://twitter.com/the_kibuika">Kibuika</a>
+        </footer>
       </div>
     </div>
   );
